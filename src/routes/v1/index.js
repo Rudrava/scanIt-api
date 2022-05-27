a61d2fb8954f9609e6aefa67b281ffb6f21245c1
Rudrava <rudravamukherjee.2001@gmail.com>
2022-05-27 22:44:30 +0530

    ADD docs to prod

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -30,10 +30,8 @@ defaultRoutes.forEach((route) => {
 });
 
 /* istanbul ignore next */
-if (config.env === 'development') {
-  devRoutes.forEach((route) => {
-    router.use(route.path, route.route);
-  });
-}
+devRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
 
 module.exports = router;
